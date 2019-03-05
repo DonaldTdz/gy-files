b1cdb631993e06aba066e91a7d009de4c7e3ebea
YangFan17 <173838362@qq.com>
2019-03-05 15:53:26 +0800

    合并

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ App({
   globalData: {
     userInfo: { id: '', name: '', position: '', avatar: '' },
     host: 'http://gyapi.intcov.com/',
-    //host: 'http://yangfan.vaiwan.com/',
+    // host: 'http://yangfan.vaiwan.com/',
     //host: 'http://127.0.0.1:21021/',
     corpId: ''
   },
